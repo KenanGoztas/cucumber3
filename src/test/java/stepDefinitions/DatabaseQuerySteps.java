e924b647c16e4f97977bbb439e1ab91b895c5c9a
Kenan Göztas <kenangoz0106@gmail.com>
2023-03-13 23:51:37 +0100

    cucumber

---
+++ src/test/java/stepDefinitions/DatabaseQuerySteps.java
@@ -0,0 +1,31 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DatabaseQuerySteps {
+    @Given("user connects to the database")
+    public void userConnectsToTheDatabase() {
+    }
+
+    @When("user connects to the student table")
+    public void userConnectsToTheStudentTable() {
+    }
+
+    @Then("print the total row count and verify")
+    public void printTheTotalRowCountAndVerify() {
+    }
+
+    @Then("print first student name and verify")
+    public void printFirstStudentNameAndVerify() {
+    }
+
+    @Then("print last student name and verify")
+    public void printLastStudentNameAndVerify() {
+    }
+
+    @Then("print spesific student name and verify")
+    public void printSpesificStudentNameAndVerify() {
+    }
+}
